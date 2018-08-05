d867248bc15c4c61fd0ee4535b3c8cb0a6c47eaa
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 18:31:49 +0300

    need to finish 1.1  1.2  1.3  1.4  1.5  1.6

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,13 @@
 # Write your solution for 1.2 here!
+#sum = 0
+#a = 0
+#for i in range(101):
+#	if i%2==0:
+#		sum = sum + i 
+#print(sum)
+#for i in range(1000):
+#	if 
+n = 10
+while n < 100:
+    n = n * 2
+    print(n)
--- exercises/while_loops.py
+++ exercises/while_loops.py
@@ -1,1 +1,20 @@
 # Write your solution for 1.3 here!
+counter = 0
+sum = 0
+while sum < 10000:
+	counter = counter + 1
+	sum = sum + counter
+print (sum)
+print (counter)
+
+
+
+
+
+
+
+
+
+
+
+
